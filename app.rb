bfff34dc00b92d0550027608d369e6e055649787
Sara Gorecki <sgorecki@gmail.com>
2013-12-19 18:23:52 -0500

    change app.rb to require dependencies and route

--- app.rb
+++ app.rb
@@ -1,27 +1,14 @@
+require "bundler"
+Bundler.require
 require_relative "./lib/board"
 require_relative "./lib/cell"
 
+module Life
+	class Game < Sinatra::Application
 
-def visualize(array)
-	array.each do |row|
-		row.each do |cell|
-			print "O" if cell.state == "alive"
-			print "." if cell.state == "dead"
+		get '/' do
+			"test"
 		end
-		puts
+
 	end
-	puts
-	puts
-end
-
-
-starting_array = [[8,8],[8,9],[8,11],[9,8],[9,9],[10,10]]
-game = Board.new(12, 20)
-game.starting_move!(starting_array)
-visualize(game.all_cells)
-50.times do
-	game.evaluate_all
-	game.tick!
-	visualize(game.all_cells)
-	sleep(1)
 end
