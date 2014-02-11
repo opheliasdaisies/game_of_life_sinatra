282c6cf26f7a5a28e6b23669656c21dce39b4a39
Sara Gorecki <sgorecki@gmail.com>
2014-02-11 18:01:13 -0500

    add route to sinatra tthat will look at angular

--- app.rb
+++ app.rb
@@ -6,6 +6,14 @@ require_relative "./lib/cell"
 module Life
 	class Game < Sinatra::Application
   
+  configure do
+    set :root, File.dirname(__FILE__)
+    set :public_folder, "public"
+  end
+
+  get "/" do
+    File.read("public/app/index.html")
+  end
 
 	end
 end
