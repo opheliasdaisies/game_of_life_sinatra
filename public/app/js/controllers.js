8e26fc925444d4ff09a54f73dc29c6210687f4a9
Sara Gorecki <sgorecki@gmail.com>
2014-02-12 11:18:55 -0500

    fix typo in controller

--- public/app/js/controllers.js
+++ public/app/js/controllers.js
@@ -4,7 +4,7 @@ var lifeControllers = angular.module("lifeControllers", []);
 
 lifeApp.controller("BoardCtrl", ["$scope", "$http",
   function($scope, $http){
-    $http.get("http://localhost:9393/random_board").success(function(data)){
+    $http.get("http://localhost:9393/random_board").success(function(data){
       $scope.board = data;
-    }
+    })
   }]);
