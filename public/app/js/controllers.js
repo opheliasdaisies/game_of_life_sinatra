b588f5bfee0e6f2d1556760059ea098b21798131
Sara Gorecki <sgorecki@gmail.com>
2014-02-12 12:01:10 -0500

    change name of board scope to be boards

--- public/app/js/controllers.js
+++ public/app/js/controllers.js
@@ -5,6 +5,6 @@ var lifeControllers = angular.module("lifeControllers", []);
 lifeApp.controller("BoardCtrl", ["$scope", "$http",
   function($scope, $http){
     $http.get("http://localhost:9393/random_board").success(function(data){
-      $scope.board = data;
+      $scope.boards = data;
     });
   }]);
