f41bd456bba205b8101b5cc748a20daa2e6e7f5b
Sara Gorecki <sgorecki@gmail.com>
2014-02-12 10:43:57 -0500

    begin building app.js

---
+++ public/app/js/app.js
@@ -0,0 +1,18 @@
+"use strict";
+
+var lifeApp = angular.module("lifeApp", [
+  "ngRoute",
+  "lifeControllers"
+  ]);
+
+lifeApp.config(["$routeProvider",
+  function($routeProvider){
+    when("/random", {
+      templateUrl: "partials/randomBoard.html",
+      controller:
+    }).
+    otherwise({
+      redirectTo: "/random"
+    });
+  }
+  ]);
