a0ba8547a5c1f477070b1b5f5c8d42bb9c032f7e
Sara Gorecki <sgorecki@gmail.com>
2014-02-13 17:09:45 -0500

    begin working on second http request to add second batch of 50 turns

--- app.rb
+++ app.rb
@@ -7,6 +7,7 @@ require_relative "./lib/cell"
 module Life
 	class Game < Sinatra::Application
     helpers Sinatra::JSON
+    enable :sessions
   
     configure do
       set :root, File.dirname(__FILE__)
@@ -27,16 +28,23 @@ module Life
         board.tick!
         turns << board.cell_states
       end
+      session[:state] = turns[-1]
 
       json turns
     end
 
-    # post '/tick_board' do
-    #   board = Board.new(10,10)
-    #   board.starting_move!(params[:board])
-    #   board.tick
-    #   json board.cell_states
-    # end
+    get '/tick_board' do
+      board = Board.new(30,40)
+      # board.populate_board(session[:state])
+      board.starting_move!(board.randomize_start)
+      turns = []
+      50.times do
+        board.evaluate_all
+        board.tick!
+        turns << board.cell_states
+      end
+      json turns
+    end
 
 	end
 end
--- public/app/js/controllers.js
+++ public/app/js/controllers.js
@@ -6,12 +6,22 @@ lifeApp.controller("BoardCtrl", ["$scope", "$http", "$timeout",
   function($scope, $http, $timeout){
     $http.get("http://localhost:4000/random_board").success(function(data){
       $scope.boards = data;
-    
+
       var nextMove = function(){
         $scope.boards.shift();
         $timeout(nextMove, 1000);
       }
 
+      var newPositions = function(){
+        if ($scope.boards.length < 10){
+          $http.get("http://localhost:4000/tick_board").success(function(data){
+            $scope.boards.concat(data);
+          });
+        }
+        $timeout(newPositions, 45000);        
+      }
+
       nextMove();
+      newPositions();
     });
   }]);
