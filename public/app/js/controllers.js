15574d7a4d62a00d0f114502fbaa39e68e36392f
Sara Gorecki <sgorecki@gmail.com>
2014-02-14 10:37:41 -0500

    add loading variable so that request for next move set won't keep running if waiting for data return. fix concat arrays.

--- public/app/js/controllers.js
+++ public/app/js/controllers.js
@@ -6,22 +6,19 @@ lifeApp.controller("BoardCtrl", ["$scope", "$http", "$timeout",
   function($scope, $http, $timeout){
     $http.get("http://localhost:4000/random_board").success(function(data){
       $scope.boards = data;
+      var loading = false;
 
       var nextMove = function(){
         $scope.boards.shift();
         $timeout(nextMove, 1000);
-      }
-
-      var newPositions = function(){
-        if ($scope.boards.length < 10){
+        if ($scope.boards.length < 20 && loading == false){
           $http.get("http://localhost:4000/tick_board").success(function(data){
-            $scope.boards.concat(data);
+            $scope.boards = $scope.boards.concat(data);
+            loading = true
           });
         }
-        $timeout(newPositions, 45000);        
       }
 
       nextMove();
-      newPositions();
     });
   }]);
