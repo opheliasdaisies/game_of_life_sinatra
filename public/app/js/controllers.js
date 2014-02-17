8dc31f4855e8bc5bd5c575d38d0d3fe34f843e13
Sara Gorecki <sgorecki@gmail.com>
2014-02-17 12:00:41 -0500

    fix url for deployment

--- public/app/js/controllers.js
+++ public/app/js/controllers.js
@@ -4,7 +4,7 @@ var lifeControllers = angular.module("lifeControllers", []);
 
 lifeApp.controller("BoardCtrl", ["$scope", "$http", "$timeout",
   function($scope, $http, $timeout){
-    $http.get("http://localhost:4000/random_board").success(function(data){
+    $http.get("/random_board").success(function(data){
       $scope.boards = data;
       var loading = false;
 
